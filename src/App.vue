f4ed5f6872c18cc8626292110857b2188ac4c97f
emcog <robin.leonard@gmail.com>
2021-01-17 01:43:35 +0000

    refactored code, data now prepped for passing to api"

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,43 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <button v-on:click="getPostcodes">01 Return postcodes</button>
-    <button v-on:click="singleCustomer">02 GET postcodes to api</button>
-    <button v-on:click="postData('http://api.postcodes.io/postcodes', {'postcodes' : ['OX49 5NU', 'M32 0JG', 'NE30 1DP']})">03 POST postcodes to api</button>
+    <button v-on:click="singleCustomer">02 GET postcodes api</button>
+    <!-- <button v-on:click="postData('http://api.postcodes.io/postcodes', '{{postcodes}}')">03 POST postcodes to api</button> -->
+    <!-- <button v-on:click="postLog('http://api.postcodes.io/postcodes', '{{postcodes}}')">04 POST postcodes to api</button> -->
+    <!-- <button v-on:click="console.log(postcodes)">log it</button> -->
+    <button v-on:click="logIt">log it</button>
+    <span>{ postcodes:{{ postcodes }}}</span>
     <!-- <button v-on:click="batchPostcode('http://api.postcodes.io/postcodes', {'postcodes' : ['OX49 5NU', 'M32 0JG', 'NE30 1DP']})">03 POST postcodes to api</button> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import postData from './js/postData.js' 
-// import postData from './js/postData.js' 
-const postcodes = require('node-postcodes.io')
 
 export default {
   data() {
     return {
       prospectsAndCustomers: [],
-      postcodes : [],
+      postcodes : {postcodes : []}, //change this to an object postcodes: postcodes {}
+      // postcodes : [], //change this to an object postcodes: postcodes {}
       dataFromPostCodes : {}
     }
   },
 
   methods: {
+    
     getPostcodes : function(prospectsAndCustomers) {
-      return this.postcodes = this.prospectsAndCustomers.map(prospectsAndCustomer => prospectsAndCustomer.Postcode)
+      let postcodesArray = this.prospectsAndCustomers.map(prospectsAndCustomer => prospectsAndCustomer.postcode)
+      return this.postcodes.postcodes = postcodesArray;
+      // how to add an array to an  object key
+      // return this.postcodes = this.prospectsAndCustomers.map(prospectsAndCustomer => prospectsAndCustomer.postcode)
+    },
 
+    logIt: function() {
+      // const postCodeString = this.postcodes.map(postcode => `${postcode}`);
+    // add "" to every array entry using map" 
+      // console.log( `"{ postcodes : " + ${postCodeString}` );
+      console.log(JSON.stringify(this.postcodes));
     },
 
       
@@ -37,7 +48,11 @@ export default {
       .then(data => console.log(data));
     },
 
-    // batchPostcode: await postcodes.lookup(['PO123AA', 'PO123AB']),
+    postLog : function(url = '', data = {}) {
+        console.log(JSON.stringify(data))
+    },
+
+
  
      postData: async function(url = '', data = {}) {
         // Default options are marked with *
@@ -54,20 +69,16 @@ export default {
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data) // body data type must match "Content-Type" header
       }).then(this.dataFromPostCodes = data);
-      // .then(console.log(data));
-      // return this.dataFromPostCodes = response.json(); // parses JSON response into native JavaScript objects
-      // console.log(response.json()); // parses JSON response into native JavaScript objects
     }
   },
-
-  // computed() {
-    
-  // },
 
   mounted() {
     fetch('./ImportedData/customers2020.json')
     .then(res => res.json())
     .then(prospectsAndCustomers => this.prospectsAndCustomers = prospectsAndCustomers )
+    .then(this.getPostcodes)
+    .then(console.log(this.postcodes))
+    // .then(this.postData("http://api.postcodes.io/postcodes", "postcodes:" this.postcodes ))
   },
 
 name: 'App',
